3aa03391141beedf58bb4116ad36c395c2e34b00
relmi27 <mirel.muslija27@gmail.com>
2016-04-13 09:28:31 +0200

    layout modifications

--- app/src/main/java/willcodeforfood/tvzmc2/feedme/activities/MainActivity.java
+++ app/src/main/java/willcodeforfood/tvzmc2/feedme/activities/MainActivity.java
@@ -3,6 +3,7 @@ package willcodeforfood.tvzmc2.feedme.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
 
 import willcodeforfood.tvzmc2.feedme.R;
 
@@ -15,4 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    // TODO Implement recycler grid view or standard gridview
+
+    // TODO Add firebase support with standard firebase sdk and firebase ui
 }

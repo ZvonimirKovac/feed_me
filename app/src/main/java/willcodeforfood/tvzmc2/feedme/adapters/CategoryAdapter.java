45e4d7af9b928fdd728f6c34521e4ac26e9021b4
relmi27 <mirel.muslija27@gmail.com>
2016-04-17 17:23:48 +0200

    implemented glide for image handling

--- app/src/main/java/willcodeforfood/tvzmc2/feedme/adapters/CategoryAdapter.java
+++ app/src/main/java/willcodeforfood/tvzmc2/feedme/adapters/CategoryAdapter.java
@@ -1,14 +1,17 @@
 package willcodeforfood.tvzmc2.feedme.adapters;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.firebase.client.Firebase;
 import com.firebase.client.utilities.Base64;
 import com.firebase.ui.FirebaseRecyclerAdapter;
@@ -27,41 +30,37 @@ public class CategoryAdapter extends FirebaseRecyclerAdapter<Category, CategoryA
     @Override
     protected void populateViewHolder(CategoryViewHolder categoryViewHolder, Category category, int i) {
         try {
-            categoryViewHolder.itemView.setBackgroundDrawable(createBitmap(category.getBase64EncodedImage()));
-            categoryViewHolder.nameView.setText(category.getCategoryName());
+            Glide.with(categoryViewHolder.context)
+                    .load(Base64.decode(category.getBase64EncodedImage()))
+                    .crossFade()
+                    .placeholder(R.drawable.placeholder_category)
+                    .into(categoryViewHolder.categoryImage);
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-    }
 
-    @NonNull
-    private BitmapDrawable createBitmap(String base64EncodedImage) throws IOException {
-        byte[] decodedImage = Base64.decode(base64EncodedImage);
-        Bitmap bitmap = BitmapFactory.decodeByteArray(decodedImage, 0, decodedImage.length);
-        return new BitmapDrawable(null, bitmap);
+        categoryViewHolder.nameView.setText(category.getCategoryName());
     }
 
     public static class CategoryViewHolder extends RecyclerView.ViewHolder {
-        View itemView;
+        Context context;
+        ImageView categoryImage;
         TextView nameView;
 
         public CategoryViewHolder(View itemView) {
             super(itemView);
-            this.itemView = itemView;
+            this.context = itemView.getContext();
+            this.categoryImage = (ImageView) itemView.findViewById(R.id.categoryImage);
             this.nameView = (TextView) itemView.findViewById(R.id.categoryNameTextView);
 
-            initListeners();
-        }
-
-        private void initListeners() {
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    String name = nameView.getText().toString();
-                    Toast.makeText(v.getContext(), name + "clicked", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(v.getContext(), "Clicked " + nameView.getText(), Toast.LENGTH_SHORT).show();
                 }
             });
         }
+
     }
 }

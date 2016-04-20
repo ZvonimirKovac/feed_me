2d3b943f207778ebbaf984e6b8cf456f086602bd
relmi27 <mirel.muslija27@gmail.com>
2016-04-20 21:21:54 +0200

    added recipe activity

---
+++ app/src/main/java/willcodeforfood/tvzmc2/feedme/activities/RecipeActivity.java
@@ -0,0 +1,15 @@
+package willcodeforfood.tvzmc2.feedme.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import willcodeforfood.tvzmc2.feedme.R;
+
+public class RecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
